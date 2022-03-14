4c7a1cc38f17cacf187413ac9c1fc1f298dda6d3
Patrick Schreiner Home <pschreiner@gmx.net>
2022-03-14 14:39:20 +0100

    .

---
+++ DTAtoSEDAS/Dat2Sedas_Tests/ConvertDatToSedas_Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dat2Sedas_Tests
+{
+    [TestClass]
+    public class ConvertDatToSedas_Tests
+    {
+        [TestMethod]
+        public void DeleteCustomerTest()
+        {
+        }
+    }
+}
